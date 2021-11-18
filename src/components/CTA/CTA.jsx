2ab61ee17fd330e51d1a63187ee39d1bdeacd43a
VaheAA <vahe.abov@gmail.com>
2021-11-18 20:30:21 +0400

    new

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -1,38 +1,43 @@
 import React, {useState} from 'react';
+import {Fragment} from 'react/cjs/react.production.min';
 import {BTN} from '..';
 import Modal from '../../containers/modal/Modal';
 
 import './cta.css';
 
-const CTA = ({fundTitle, amount, fundText, count}) => {
+const CTA = ({fundTitle, amount, fundText, count, isOpen}) => {
   const [isShown, setShown] = useState(false);
   const openModal = () => {
     setShown(!isShown);
   };
   return (
-    <div className={count === '0' ? 'cta cta--disabled' : 'cta'}>
-      <div className="container-xsm">
-        <div className="cta__inner">
-          <div className="cta__heading">
-            <h5 className="cta__title">{fundTitle}</h5>
-            <p className="cta__amount">Pledge ${amount} or more</p>
-          </div>
-          <div className="cta__desc">
-            <p>{fundText}</p>
-          </div>
-          <div className="cta__info">
-            <p className="cta__count">
-              {count} <span>left</span>
-            </p>
-            <BTN
-              text={count === '0' ? 'Out of Stock' : 'Select reward'}
-              onclick={() => openModal()}
-            />
+    <Fragment style={{position: 'relative'}}>
+      <div
+        className={count === '0' ? 'cta cta--disabled' : 'cta'}
+        status={isOpen}>
+        <div className="container-xsm">
+          <div className="cta__inner">
+            <div className="cta__heading">
+              <h5 className="cta__title">{fundTitle}</h5>
+              <p className="cta__amount">Pledge ${amount} or more</p>
+            </div>
+            <div className="cta__desc">
+              <p>{fundText}</p>
+            </div>
+            <div className="cta__info">
+              <p className="cta__count">
+                {count} <span>left</span>
+              </p>
+              <BTN
+                text={count === '0' ? 'Out of Stock' : 'Select reward'}
+                openModal={openModal}
+              />
+            </div>
           </div>
         </div>
       </div>
       {isShown ? <Modal /> : ''}
-    </div>
+    </Fragment>
   );
 };
 
--- src/components/btn/BTN.jsx
+++ src/components/btn/BTN.jsx
@@ -3,12 +3,13 @@ import {Fragment} from 'react';
 
 import './btn.css';
 
-const BTN = ({text, disabled}) => {
+const BTN = ({text, disabled, openModal}) => {
   return (
     <Fragment>
       <button
         className="btn btn__fund"
-        style={disabled ? {pointerEvents: 'none', opacity: '0.4'} : {}}>
+        style={disabled ? {pointerEvents: 'none', opacity: '0.4'} : {}}
+        onClick={openModal}>
         {text}
       </button>
     </Fragment>
